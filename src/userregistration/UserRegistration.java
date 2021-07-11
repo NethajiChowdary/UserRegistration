83002961f57d442ee3057da3115cb3fb68e4d718
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-11 21:22:06 +0530

    Added: Code to Follow Pre-Defined Password Rules by the User

--- src/userregistration/UserRegistration.java
+++ src/userregistration/UserRegistration.java
@@ -2,30 +2,30 @@ package userregistration;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class UserRegistration
+class UserRegistration
 {
 
     Scanner scanner = new Scanner(System.in);
 
-    public static final String NUMBERPATTERN = "^[+0-9]{2}{10}$";
+    public static final String PASSWORDPATTERN = "[0-9a-zA-Z!@#$%^&*]{8,}";
 
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         UserRegistration userRegistration = new UserRegistration();
         userRegistration.readInput();
     }
-    //Method to read the input from the user
-    private void readInput()
+  //method to read the input from the user
+    private void readInput() 
     {
-        System.out.println("Enter Phone Number ");
-        String PhoneNumber = scanner.next();
-        validateInput(NUMBERPATTERN,PhoneNumber);
+        System.out.println("Enter The Password");
+        String Password = scanner.next();
+        validateInput(PASSWORDPATTERN,Password);
     }
 
-    private void validateInput(String pattern, String input) 
+    private void validateInput(String pattern, String input)
     {
 
-        if(Pattern.matches(pattern, input)) 
+        if(Pattern.matches(pattern, input))
         {
             System.out.println("Valid");
         }
